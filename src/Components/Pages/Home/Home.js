855acd312715d72ba983de8a470b05691b99c371
Ahammed Fahim Galib <galib0124gmail.com>
2022-12-28 18:06:49 +0600

    task complete

--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { toast } from 'react-hot-toast';
 import { useSelector } from 'react-redux';
 import { selectUser } from '../../../features/counter/counterSlice';
@@ -13,12 +13,13 @@ const Home = () => {
     }
 
     const handlesubmit = event => {
+         setSubmit(task);
 
         if(event.key === 'Enter'){
-            setSubmit(task);
         const postData = {
                 taskData: submit,
-                email: user.email
+                email: user.email,
+                name: user.displayName
             }
             fetch('http://localhost:5000/allTask', {
                 method: 'POST',
@@ -30,7 +31,7 @@ const Home = () => {
             .then(res => res.json())
             .then(data => {console.log(data)
                 if(data.acknowledged){
-                    toast.success('Task is Submitted')
+                    toast.success('Task is Submitted');
                 }
             })  
             
@@ -42,10 +43,11 @@ const Home = () => {
     return (
         <div>
             <h1 className='text-slate-500 dark:text-slate-400 text-center text-2xl font-bold mt-24'>Start With Your Task</h1>
-            <form onKeyDown={handlesubmit} className='px-3 lg:w-3/5 mx-auto mt-14'>
+            <form  className='px-3 lg:w-3/5 mx-auto mt-14'>
                 <div className="w-full border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
                     <div className="px-4 py-2 bg-white rounded-t-lg dark:bg-gray-800">
-                        <textarea value={task} onChange={submitHandler} name='taskdescription' id="comment" rows="12" className="w-full px-0 text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400 text-md" placeholder="Write your task..." required></textarea>
+                        {/* <textarea value={task}  name='taskdescription' id="comment" rows="12" className="w-full px-0 text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400 text-md" placeholder="Write your task..." required></textarea> */}
+                        <input onKeyDown={handlesubmit} onChange={submitHandler} placeholder="give a task" type="text" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
                     </div>
                 </div>
             </form>
